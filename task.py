4c129c21cb34ada549cf5ababda8e0cf13adf696
DEYVID BARCELOS <deyvid_barcelos@estudante.sesisenai.org.br>
2025-07-31 11:03:40 -0300

    Task Plotted

--- task.py
+++ task.py
@@ -1,4 +1,20 @@
-from numpy import array, mean, std
+import numpy as np
+from numpy import mean, std, array
+import pandas as pd
+import plotly.express as px
 
-tempos_ciclo = array([5.5, 5.7, 5.9, 6.0, 5.8, 5.6, 5.7, 7.2, 4.8])
-print(tempos_ciclo[(tempos_ciclo < mean(tempos_ciclo) - 2 * std(tempos_ciclo)) | (tempos_ciclo > mean(tempos_ciclo) + 2 * std(tempos_ciclo))])
+data_array = array([5.5, 5.7, 5.9, 6.0, 5.8, 5.6, 5.7, 7.2, 4.8])
+
+category = ['Outlier' if (val < mean(data_array) - 2 * std(data_array) or val > mean(data_array) + 2 * std(data_array)) else 'Normal' for val in data_array]
+
+df = pd.DataFrame({
+    'Time (minutes)': data_array,
+    'Values': range(1, len(data_array) + 1),
+    'category': category
+})
+
+fig = px.line(df, x='Values', y='Time (minutes)', color='category', template='plotly_dark',
+              title='Tempos de Ciclo',
+              markers=True)
+
+fig.show()
