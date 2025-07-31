409570ab3d29635bdc2bc888a78a848f5ce3dfdc
KAUÃ MARTINS DE SOUZA <kaua_martins-souza@estudante.sesisenai.org.br>
2025-07-31 09:15:02 -0300

    Teste

--- grupoexmesa.py
+++ grupoexmesa.py
@@ -1,4 +1,23 @@
 #kaua
 #adrian
 #lucas#qualquer coisa
-nome = input("Informe o nome: ")
+
+
+import numpy as np
+
+# Salários dos funcionários
+salarios = np.array([3000, 3500, 4000, 2000, 4500, 4000, 5000])
+
+# Calcular a média salarial
+media_salarial = np.mean(salarios)
+
+print(media_salarial)
+
+def grupo():
+    funcionarios_acima = 0
+media = np.mean(salarios)
+funcionarios_acima = np.sum(salarios > media)
+print(f"A média salarial é: {media:.2f}")
+print(f"O número de funcionários com salário acima da média é: {funcionarios_acima}")
+
+grupo()
